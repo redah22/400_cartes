5c8292e5f14a2ae9a9972e1d7ad14bf84598fe3b
redah22 <124052549+redah22@users.noreply.github.com>
2026-04-28 16:25:35 +0200

    link back

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.MODE === 'production' ? undefined : 'http://localhost:3000';
+const URL = import.meta.env.MODE === 'production' ? 'https://four00-cartes.onrender.com' : 'http://localhost:3000';
 
 export const socket = io(URL as string, {
   autoConnect: false
